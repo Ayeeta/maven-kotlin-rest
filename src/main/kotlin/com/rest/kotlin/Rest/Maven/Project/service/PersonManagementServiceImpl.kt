3ada1d8072e20a92d23b19111bd944d951573f8a
Elijah <elijate@gmail.com>
2020-09-14 22:31:17 +0300

    Add service

---
+++ src/main/kotlin/com/rest/kotlin/Rest/Maven/Project/service/PersonManagementServiceImpl.kt
@@ -0,0 +1,30 @@
+package com.rest.kotlin.Rest.Maven.Project.service
+
+import com.rest.kotlin.Rest.Maven.Project.dao.PersonDAO
+import com.rest.kotlin.Rest.Maven.Project.dto.AddPersonRequest
+import com.rest.kotlin.Rest.Maven.Project.dto.PersonResponse
+import com.rest.kotlin.Rest.Maven.Project.dto.UpdatePersonRequest
+import org.springframework.stereotype.Service
+
+@Service
+class PersonManagementServiceImpl(private val personDAO: PersonDAO) : PersonManagementService {
+    override fun findById(id: Long): PersonResponse? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAll(): List<PersonResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(addPersonRequest: AddPersonRequest): PersonResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(updatePersonRequest: UpdatePersonRequest): PersonResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteById(id: Long) {
+        TODO("Not yet implemented")
+    }
+}
